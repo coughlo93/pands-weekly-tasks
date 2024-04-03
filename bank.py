1b307ec443061e7e8d0008b0fd517b2b538b0483
Owen Coughlan <owenc1993@gmail.com>
2024-04-03 15:03:19 +0100

    commit

--- bank.py
+++ bank.py
@@ -1,21 +1,16 @@
 def main():
-    
-    # Input the first amount in cents
-    amount1 = int(input("Enter amount 1 (in cent): "))
 
-    # Input the second amount in cents
-    amount2 = int(input("Enter amount 2 (in cent): "))
-
-    # Calculate the total amount
+    # Input the first money amount in cents
+    amount1 = int(input("Enter amount1 (in cent): "))
+    # Input the second money amount in cents
+    amount2 = int(input("Enter amount2 (in cent): "))
+    # Total amount
     total_amount = amount1 + amount2
-
-    # Convert to euros and cents
+    # Convert the total amount to euros and cents
     euros = total_amount // 100
     cents = total_amount % 100
-
     # Print answer
     print(f"The sum of these is €{euros}.{cents:02d}")
 
 if __name__ == "__main__":
     main()
-    
